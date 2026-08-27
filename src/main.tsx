617d2d9087829cfbaf0504321943374a1db9581e
felipeads12 <lipepassos180208gmail.com>
2026-08-27 11:27:28 -0300

    Feat: rotas de Home, Locais, Cadastro e Sobre feitas.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {route} from "./routes"
+import { route } from "./routes/routes"
 import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
@@ -9,3 +9,5 @@ createRoot(document.getElementById('root')!).render(
     <RouterProvider router={route}/>
   </StrictMode>,
 )
+
+
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import App from './App'
-
+import { createBrowserRouter } from "react-router-dom"
+import App from '../App'
+import Home from '../pages/Home/Home'
+import Locais from '../pages/Locais/Locais'
+import Cadastro from '../pages/Cadastro/Cadastro'
+import Sobre from '../pages/Sobre/Sobre'
 
 export const route = createBrowserRouter([
     {path: "/", element : <App/>,
         children:[
-            
+            { index: true, element: <Home/> },
+            { path: "locais", element: <Locais/> },
+            { path: "cadastrar", element: <Cadastro/> },
+            { path: "sobre", element: <Sobre/> },
         ]
     }
 ])
-
-
-
