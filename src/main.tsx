b7fb3adf90c50f84990c5813dcbd5f64b2c2432a
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-08-31 21:16:45 -0300

    refact: arruma routes para funcionar com react-router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 import { createBrowserRouter,RouterProvider } from 'react-router';
 
-const router = createBrowserRouter([ ]);
+// Import das rotas
+import Home from  './routes/Home/index.tsx';
+import Locais from './routes/Locais/index.tsx';
+import Cadastro from './routes/Cadastro/index.tsx';
+import Sobre from './routes/Sobre/index.tsx';
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, children: [
+    {path:"/", element:<Home/>},
+    {path:"/locais", element:<Locais/>},
+    {path:"/cadastrar", element:<Cadastro/>},
+    {path:"/sobre", element:<Sobre/>},
+  ]}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
