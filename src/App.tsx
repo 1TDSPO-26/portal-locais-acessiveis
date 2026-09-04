f477f6deb58c8291c15dc535fb965904eb77da62
Guilherme Dos Santos Rocha <gui23rocha@gmail.com>
2026-09-04 14:23:40 -0300

    feat: implementar Footer acessivel com NavLink, foco visivel e semantica #23

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./layouts/MainLayout/MainLayout";
-import { Outlet } from "react-router";
+import Home from "./routes/Home";
+import Locais from "./routes/Locais";
+import Cadastro from "./routes/Cadastro";
+import Sobre from "./routes/Sobre";
 
 export default function App() {
   return (
-    <MainLayout>
-      <Outlet />
-    </MainLayout>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/locais" element={<Locais />} />
+          <Route path="/cadastrar" element={<Cadastro />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import type { ReactNode } from "react";
 
-// Define a interface para as Props do Layout
-interface MainLayoutProps {
-  children: ReactNode;
-}
-
-export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+export function MainLayout() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col">
       <Header />
-
-      <main>{children}</main>
-
+      <main className="flex-1">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
-};
+}
