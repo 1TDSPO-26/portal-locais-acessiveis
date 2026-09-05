486b0afcf8fc0f42415730d3b0ec48a289e450b6
Enzo Negrão Freitas <enzo_negrao_freitas@outlook.com>
2026-09-04 21:20:18 -0300

    fix: reverte arquivo para estado original (develop)
    Arquivo alterado desnecessariamente.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "./layouts/MainLayout/MainLayout";
-import Home from "./routes/Home";
-import Locais from "./routes/Locais";
-import Cadastro from "./routes/Cadastro";
-import Sobre from "./routes/Sobre";
+import { Outlet } from "react-router";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/locais" element={<Locais />} />
-          <Route path="/cadastrar" element={<Cadastro />} />
-          <Route path="/sobre" element={<Sobre />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
   );
 }
