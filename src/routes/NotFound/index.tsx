e71165eda661dd00672d80a27399b2fb4e1d4d7a
alexandreteixeiras <teixeiradossantosalexandre@gmail.com>
2026-09-07 18:55:25 -0300

    feat: adicionando recurso de acessibilidade no botão da página.

--- src/routes/NotFound/index.tsx
+++ src/routes/NotFound/index.tsx
@@ -7,7 +7,9 @@ export default function NotFound() {
                     <h1 className="mb-3 text-4xl font-bold leading-none !text-[#0052B4] sm:text-5xl">404</h1>
                     <h2 className="mb-3 text-2xl font-bold leading-tight tracking-tight !text-[#0F172A] sm:text-3xl">Página não encontrada</h2>
                     <p className="mb-6 text-base leading-6 text-[#475569]">O endereço que você tentou acessar não existe ou foi alterado.</p>
-                    <a href="/" className="inline-flex min-h-10 items-center justify-center rounded-md bg-[#005FCC] px-4 py-2 text-sm font-semibold text-[#FFFFFF] transition-colors hover:bg-[#005FCC] focus-visible:outline-3 focus-visible:outline-offset-3 focus-visible:outline-[#0F172A] ">Voltar para o início</a>
+                    <button title="Botão para retornar a página inicial">
+                        <a href="/" className="inline-flex min-h-10 items-center justify-center rounded-md bg-[#005FCC] px-4 py-2 text-sm font-semibold text-[#FFFFFF] transition-colors hover:bg-[#005FCC] focus-visible:outline-3 focus-visible:outline-offset-3 focus-visible:outline-[#0F172A] ">Voltar para o início</a>
+                    </button>
                 </div>
             </section>
         </main>
