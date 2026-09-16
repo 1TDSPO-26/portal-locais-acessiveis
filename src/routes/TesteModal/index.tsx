266cc1a15cdcc38ce14f5e5c7743ac44b13bc70b
Gracetti <igor.gra7@gmail.com>
2026-09-15 22:01:08 -0300

    feat criacao de rota para visualizar como ficou o modal

---
+++ src/routes/TesteModal/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ModalConfirmacao from '../../components/ModalConfirmacao/ModalConfirmacao'
+
+export default function 
+() {
+  return (
+    <div>
+        <ModalConfirmacao/>
+    </div>
+  )
+}
